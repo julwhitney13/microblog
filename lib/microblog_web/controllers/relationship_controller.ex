1e773077c810f7d251f589ad406db6473669bacc
Kim <whitney.k@husky.neu.edu>
2017-10-02 03:19:29 -0400

    commen out

--- lib/microblog_web/controllers/relationship_controller.ex
+++ lib/microblog_web/controllers/relationship_controller.ex
@@ -4,50 +4,50 @@ defmodule MicroblogWeb.RelationshipController do
   alias Microblog.Accounts
   alias Microblog.Accounts.Relationship
 
-  def index(conn, _params) do
-    relationships = Accounts.list_relationships()
-    render(conn, "index.html", relationships: relationships)
-  end
-
-  def new(conn, _params) do
-    changeset = Accounts.change_relationship(%Relationship{})
-    render(conn, "new.html", changeset: changeset)
-  end
-
-  def create(conn, %{"relationship" => relationship_params}) do
-    case Accounts.create_relationship(relationship_params) do
-      {:ok, relationship} ->
-        conn
-        |> put_flash(:info, "Relationship created successfully.")
-        |> redirect(to: relationship_path(conn, :show, relationship))
-      {:error, %Ecto.Changeset{} = changeset} ->
-        render(conn, "new.html", changeset: changeset)
-    end
-  end
-
-  def show(conn, %{"id" => id}) do
-    relationship = Accounts.get_relationship!(id)
-    render(conn, "show.html", relationship: relationship)
-  end
-
-  def edit(conn, %{"id" => id}) do
-    relationship = Accounts.get_relationship!(id)
-    changeset = Accounts.change_relationship(relationship)
-    render(conn, "edit.html", relationship: relationship, changeset: changeset)
-  end
-
-  def update(conn, %{"id" => id, "relationship" => relationship_params}) do
-    relationship = Accounts.get_relationship!(id)
-
-    case Accounts.update_relationship(relationship, relationship_params) do
-      {:ok, relationship} ->
-        conn
-        |> put_flash(:info, "Relationship updated successfully.")
-        |> redirect(to: relationship_path(conn, :show, relationship))
-      {:error, %Ecto.Changeset{} = changeset} ->
-        render(conn, "edit.html", relationship: relationship, changeset: changeset)
-    end
-  end
+  # def index(conn, _params) do
+  #   relationships = Accounts.list_relationships()
+  #   render(conn, "index.html", relationships: relationships)
+  # end
+  #
+  # def new(conn, _params) do
+  #   changeset = Accounts.change_relationship(%Relationship{})
+  #   render(conn, "new.html", changeset: changeset)
+  # end
+  #
+  # def create(conn, %{"relationship" => relationship_params}) do
+  #   case Accounts.create_relationship(relationship_params) do
+  #     {:ok, relationship} ->
+  #       conn
+  #       |> put_flash(:info, "Relationship created successfully.")
+  #       |> redirect(to: relationship_path(conn, :show, relationship))
+  #     {:error, %Ecto.Changeset{} = changeset} ->
+  #       render(conn, "new.html", changeset: changeset)
+  #   end
+  # end
+  #
+  # def show(conn, %{"id" => id}) do
+  #   relationship = Accounts.get_relationship!(id)
+  #   render(conn, "show.html", relationship: relationship)
+  # end
+  #
+  # def edit(conn, %{"id" => id}) do
+  #   relationship = Accounts.get_relationship!(id)
+  #   changeset = Accounts.change_relationship(relationship)
+  #   render(conn, "edit.html", relationship: relationship, changeset: changeset)
+  # end
+  #
+  # def update(conn, %{"id" => id, "relationship" => relationship_params}) do
+  #   relationship = Accounts.get_relationship!(id)
+  #
+  #   case Accounts.update_relationship(relationship, relationship_params) do
+  #     {:ok, relationship} ->
+  #       conn
+  #       |> put_flash(:info, "Relationship updated successfully.")
+  #       |> redirect(to: relationship_path(conn, :show, relationship))
+  #     {:error, %Ecto.Changeset{} = changeset} ->
+  #       render(conn, "edit.html", relationship: relationship, changeset: changeset)
+  #   end
+  # end
 
   def get_or_create(conn, %{"id" => id}) do
       case relationship = Accounts.get_or_create_relationship(id) do
