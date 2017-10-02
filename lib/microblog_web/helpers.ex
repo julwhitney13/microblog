62d618ab14eac5bafe8a2a0b995d8587ebb990bc
Kim <whitney.k@husky.neu.edu>
2017-10-01 20:14:42 -0400

    fix for now

--- lib/microblog_web/helpers.ex
+++ lib/microblog_web/helpers.ex
@@ -1,11 +1,11 @@
 defmodule MicroblogWeb.Helpers do
 
-    def user_has_posts(user) do
+    def user_has_posts(%User{} = user) do
         u = Microblog.Repo.preload(user, :posts)
         length(u.posts) > 0
     end
 
-    def get_posts(user) do
+    def get_posts(%User{} = user) do
         u = Microblog.Repo.preload(user, :posts)
         u.posts
     end
