750fc52fea70488c77273449a461bebc67d8ccb9
Kim <whitney.k@husky.neu.edu>
2017-10-02 21:08:56 -0400

    fixin up

--- lib/microblog_web/controllers/relationship_controller.ex
+++ lib/microblog_web/controllers/relationship_controller.ex
@@ -4,18 +4,6 @@ defmodule MicroblogWeb.RelationshipController do
   alias Microblog.Accounts
   alias Microblog.Accounts.Relationship
 
-  # def index(conn, _params) do
-  #   relationships = Accounts.list_relationships()
-  #   render(conn, "index.html", relationships: relationships)
-  # end
-  #
-  # def new(conn, _params) do
-  #   changeset = Accounts.change_relationship(%Relationship{})
-  #   render(conn, "new.html", changeset: changeset)
-  # end
-  #
-  # def create(conn, %{"relationship" => relationship_params}) do
-
   def create(conn, %{"relationship" => relationship_params}) do
     case Accounts.create_relationship(relationship_params) do
       {:ok, relationship} ->
@@ -29,49 +17,12 @@ defmodule MicroblogWeb.RelationshipController do
     end
   end
 
-  # def show(conn, %{"id" => id}) do
-  #   relationship = Accounts.get_relationship!(id)
-  #   render(conn, "show.html", relationship: relationship)
-  # end
-  #
-  # def edit(conn, %{"id" => id}) do
-  #   relationship = Accounts.get_relationship!(id)
-  #   changeset = Accounts.change_relationship(relationship)
-  #   render(conn, "edit.html", relationship: relationship, changeset: changeset)
-  # end
-  #
-  # def update(conn, %{"id" => id, "relationship" => relationship_params}) do
-  #   relationship = Accounts.get_relationship!(id)
-  #
-  #   case Accounts.update_relationship(relationship, relationship_params) do
-  #     {:ok, relationship} ->
-  #       conn
-  #       |> put_flash(:info, "Relationship updated successfully.")
-  #       |> redirect(to: relationship_path(conn, :show, relationship))
-  #     {:error, %Ecto.Changeset{} = changeset} ->
-  #       render(conn, "edit.html", relationship: relationship, changeset: changeset)
-  #   end
-  # end
-
-  # def get_or_create(conn, %{"id" => id}) do
-  #     case Accounts.get_or_create_relationship(id) do
-  #       {:ok, relationship} ->
-  #         conn
-  #         |> put_flash(:info, "User followed successfully.")
-  #       #   |> redirect(to: user_path(conn, :show, relationship.receiver_id))
-  #       {:error, %Ecto.Changeset{} = changeset} ->
-  #        conn
-  #         |> put_flash(:error, "Error unfollowing user.")
-  #       #   |> redirect(to: user_path(conn, :show, relationship.receiver_id))
-  #     end
-  # end
-
   def delete(conn, %{"relationship" => relationship_params}) do
     relationship = Accounts.get_relationship(relationship_params["actor_id"], relationship_params["receiver_id"])
     {:ok, _relationship} = Accounts.delete_relationship(relationship)
 
     conn
     |> put_flash(:info, "User unfollowed successfully.")
-    |> redirect(to: user_path(conn, :show, relationship.receiver_id))
+    |> redirect(to: user_path(conn, :show, relationship_params["receiver_id"]))
   end
 end
