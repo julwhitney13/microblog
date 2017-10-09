1573ba65be3948858ac29aed2755c2f4b58650bf
Kim <whitney.k@husky.neu.edu>
2017-10-08 20:51:08 -0400

    f

--- lib/microblog/messages/messages.ex
+++ lib/microblog/messages/messages.ex
@@ -124,7 +124,7 @@ defmodule Microblog.Messages do
   end
 
   def user_has_liked?(user_id, post_id) do
-      !!Repo.get(from l in Like, where: l.user_id == ^user_id && l.post_id == ^post_id)
+      !!Repo.get_by(Like, user_id: user_id, post_id: post_id)
   end
 
   @doc """
