0bb94925447e6ef3494f6e5d56202617b732e0c9
Kim <whitney.k@husky.neu.edu>
2017-10-22 18:15:09 -0400

    Continue to add security

--- lib/microblog/accounts/user.ex
+++ lib/microblog/accounts/user.ex
@@ -11,6 +11,15 @@ defmodule Microblog.Accounts.User do
     field :username, :string
     field :description, :string
 
+    field :password_hash, :string
+    field :pw_tries, :integer
+    field :pw_last_try, :utc_datetime
+
+    field :password, :string, virtual: true
+    field :password_confirmation, :string, virtual: true
+
+    field :propic, Microblog.Propic.Type
+
     has_many :posts, Microblog.Messages.Post
     has_many :following, Microblog.Accounts.Relationship, foreign_key: :actor_id
     has_many :followers, Microblog.Accounts.Relationship, foreign_key: :receiver_id
@@ -22,7 +31,34 @@ defmodule Microblog.Accounts.User do
   @doc false
   def changeset(%User{} = user, attrs) do
     user
-    |> cast(attrs, [:firstname, :lastname, :username, :email, :description])
-    |> validate_required([:firstname, :lastname, :username, :email, :description])
+    |> cast(attrs, [:firstname, :lastname, :username, :email, :description, :password, :password_confirmation])
+    |> cast_attachments(attrs, [:propic])
+    |> validate_confirmation(:password)
+    |> validate_password(:password)
+    |> put_pass_hash()
+    |> validate_required([:firstname, :lastname, :username, :email, :description, :password_hash])
   end
+
+    # Password validation
+    # From Comeonin docs
+    def validate_password(changeset, field, options \\ []) do
+      validate_change(changeset, field, fn _, password ->
+        case valid_password?(password) do
+          {:ok, _} -> []
+          {:error, msg} -> [{field, options[:message] || msg}]
+        end
+      end)
+    end
+
+    def put_pass_hash(%Ecto.Changeset{valid?: true, changes: %{password: password}} = changeset) do
+      change(changeset, Comeonin.Argon2.add_hash(password))
+    end
+
+    def put_pass_hash(changeset), do: changeset
+
+    def valid_password?(password) when byte_size(password) > 7 do
+      {:ok, password}
+    end
+
+    def valid_password?(_), do: {:error, "The password is too short"}
 end
--- lib/microblog_web/controllers/session_controller.ex
+++ lib/microblog_web/controllers/session_controller.ex
@@ -3,8 +3,45 @@ defmodule MicroblogWeb.SessionController do
 
     alias Microblog.Accounts
 
-    def login(conn, %{"email" => email}) do
+    def update_tries(throttle, prev) do
+        if throttle do
+            prev + 1
+        else
+            1
+        end
+    end
+
+    def throttle_attempts(user) do
+        y2k = DateTime.from_naive!(~N[2000-01-01 00:00:00], "Etc/UTC")
+        prv = DateTime.to_unix(user.pw_last_try || y2k)
+        now = DateTime.to_unix(DateTime.utc_now())
+        thr = (now - prv) < 3600
+
+        if (thr && user.pw_tries > 5) do
+            nil
+        else
+            changes = %{
+                pw_tries: update_tries(thr, user.pw_tries),
+                pw_last_try: DateTime.utc_now(),
+            }
+            IO.inspect(user)
+            {:ok, user} = Ecto.Changeset.cast(user, changes, [:pw_tries, :pw_last_try])
+            |> Microblog.Repo.update
+            user
+        end
+    end
+
+    def get_and_auth_user(email, password) do
         user = Accounts.get_user_by_email(email)
+        user = throttle_attempts(user)
+        case Comeonin.Argon2.check_pass(user, password) do
+          {:ok, user} -> user
+          _else       -> nil
+        end
+    end
+
+    def login(conn, %{"email" => email, "password" => password}) do
+        user = get_and_auth_user(email, password)
 
         if user do
             conn
@@ -14,7 +51,7 @@ defmodule MicroblogWeb.SessionController do
         else
             conn
             |> put_session(:user_id, nil)
-            |> put_flash(:error, "No such user")
+            |> put_flash(:error, "Incorrect email or password")
             |> redirect(to: post_path(conn, :index))
         end
     end
